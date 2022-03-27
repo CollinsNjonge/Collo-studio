9817b348383f9103988dc438b15343147073f447
CollinNjonge <collinsnjonge@gmail.com>
2022-03-27 23:20:30 +0300

    add javascript code

---
+++ js/scripts.js
@@ -0,0 +1,70 @@
+var contactForm = $("#contact-form");
+
+$(document).ready(function () {
+  // what we do titles add bold class and center
+  $(".w-title").addClass("text-center");
+
+  contactForm.on("submit", function (e) {
+    e.preventDefault();
+    let formValid = validateContactForm();
+    if (formValid) {
+      let name = $("input.name").val();
+
+      alert(
+        `Thank you ${name}, your message has been received, We'll get back to you as soon as possible.`
+      );
+      clearForm();
+    }
+  });
+
+
+  toggleWhatWeDo();
+});
+
+  // function
+
+  //  function to toggle what we do
+  function toggleWhatWeDo() {
+    $(".service").on("click", function() {
+      if ($(".service-details", this).is(":visible")) {
+        $(".service-details", this).hide();
+        $(".service-card", this).fadeIn("slow");
+      } else {
+        $(".service-details", this).fadeIn("slow");
+        $(".service-card", this).hide();
+      }
+    });
+  }
+
+
+
+  // validate contact form
+
+  function validateContactForm() {
+    let isFormValid = true;
+    const formInputs = contactForm.find("input,textarea");
+
+    formInputs.each(function() {
+      if (!$(this).val()) {
+        $(this).addClass("is-invalid");
+        isFormValid = false;
+      }
+    });
+    return isFormValid;
+  }
+
+  function removeFormErrors() {
+    contactForm.find("input,textarea").on("keydown", function () {
+      if ($(this).hasClass("is-invalid")) {
+        $(this).removeClass("is-invalid");
+      }
+    });
+  }
+
+  function clearForm() {
+    contactForm.find("input,textarea").each(function () {
+      $(this).val("");
+    });
+  }
+
+  removeFormErrors();
